ec425776e5a54eba04f521377c980dff8106fc73
VladKishkurno <rmsi12345@gmail.com>
2019-01-22 18:51:02 +0300

    AL3-P3/3. GuessType. AL3-P1/3.StaticUniqueId

--- Advanced-Lesson-3-Static-Interface/Practice.cs
+++ Advanced-Lesson-3-Static-Interface/Practice.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.Drawing.Imagine;
 
 namespace Advanced_Lesson_3_Static_Interface
 {
+
     public partial class Practice
     {
+        public class PracticeId
+        {
+            private static int _id;
+            public string Name;
+
+            public static int Id
+            {
+                get
+                {
+                    return _id;
+                }
+            }
+            static PracticeId()
+            {
+                _id = 1000;
+            }
+
+            public PracticeId()
+            {
+                _id++;
+            }
+        }
         /// <summary>
         /// AL3-P1/3. Создать класс UniqueItem c числовым полем Id. 
         /// Каждый раз, когда создается новый экземпляр данного класса, 
@@ -16,6 +41,11 @@ namespace Advanced_Lesson_3_Static_Interface
         /// </summary>
         public static void AL3_P1_3()
         {
+            for (int i = 0; i < 100; i++)
+            {
+                PracticeId a = new PracticeId();
+            }
+            Console.WriteLine(PracticeId.Id);
         }
 
         /// <summary>
@@ -27,6 +57,27 @@ namespace Advanced_Lesson_3_Static_Interface
 
         }
 
+        public static void GuessType<T>(T item)
+        {
+            switch (item)
+            {
+                case string str:
+                    Console.WriteLine($"string :  {str} ");
+                    break;
+                case int i:
+                    Console.WriteLine($"int : {i} ");
+                    break;
+                case double d:
+                    Console.WriteLine($"double : {d}");
+                    break;
+                case DateTime dateTime:
+                    Console.WriteLine($"datetime :  {dateTime}");
+                    break;
+                default:
+                    Console.WriteLine("Type is not valid");
+                    break;
+            }
+        }
 
         /// <summary>
         /// AL3-P3/3. Создайте обобщенный метод GuessType<T>(T item), 
@@ -35,6 +86,14 @@ namespace Advanced_Lesson_3_Static_Interface
         /// </summary>
         public static void AL3_P3_3()
         {
+            string text = "ssasvsdgvsa";
+            GuessType(text);
+            GuessType(5);
+            GuessType(7.45);
+            DateTime dateTime = DateTime.Now;
+            GuessType(dateTime);
+            PracticeId practice = null;
+            GuessType(practice);
         }
 
     }    
